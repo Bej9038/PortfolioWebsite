45c02f734deb250559da37f06aef0a031a74aa09
Ben Jordan <bej9038@rit.edu>
2021-05-14 22:00:54 -0400

    Added skills section

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -8,7 +8,10 @@ function Skills({skill, progress}) {
                 <div className="skill-bar">
                     <p className="skill-text">{progress}</p>
                     <div className="skill-progress">
-                        <div className="progressbar"></div>
+                        <div className="progressbar">
+                            <div className="inner-progress" style={{width: progress}}>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -9,7 +9,21 @@ function AboutPage() {
             <Title title={'About Me'} span={'About Me'} />
             <ImageSection />
             <Title title={'Skills'} span={'Skills'} />
-            <Skills skill = {'JavaScript'} progress={'70%'}/>
+            <div className="skills-group">
+                <Skills skill = {'Web Audio API'} progress={'100%'}/>
+                <Skills skill = {'HTML'} progress={'90%'}/>
+                <Skills skill = {'Git'} progress={'90%'}/>
+                <Skills skill = {'JavaScript'} progress={'90%'}/>
+                <Skills skill = {'Java'} progress={'80%'}/>
+                <Skills skill = {'CSS/SCSS'} progress={'80%'}/>
+                <Skills skill = {'C'} progress={'80%'}/>
+                <Skills skill = {'Audio Engineering'} progress={'80%'}/>
+                <Skills skill = {'SQL'} progress={'70%'}/>
+                <Skills skill = {'PHP'} progress={'70%'}/>
+                <Skills skill = {'Python'} progress={'70%'}/>
+                <Skills skill = {'React'} progress={'60%'}/>
+            </div>
+
         </div>
     );
 }
