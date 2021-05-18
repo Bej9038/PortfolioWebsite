efe6178a6d8ad78a5095800d2cc44edeec7acfdd
Ben Jordan <bej9038@rit.edu>
2021-05-18 07:49:26 -0400

    Used hash router instead of browser router

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ function App() {
       <div className="main-content">
         <div className="content">
             <Switch>
-                <Route path="/PortfolioWebsite" exact>
+                <Route path="/" exact>
                     <HomePage />
                 </Route>
 
