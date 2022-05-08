0b64a03cdc8a2fee40805351c12a8985ab9c0c89
Ben Jordan <bej9038@rit.edu>
2022-05-08 15:10:53 -0400

    Fixed contactpage.js map issue

--- src/Pages/ContactPage.js
+++ src/Pages/ContactPage.js
@@ -16,8 +16,9 @@ function ContactPage() {
             <div className="sections">
                 <div className="map-sect">
                     <iframe
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d115811.33009360616!2d-77.75030951835278!3d43.07788390741102!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d14c603a147e55%3A0xbe7eb31ed5e22c03!2sRochester%20Institute%20of%20Technology!5e0!3m2!1sen!2sus!4v1647568990533!5m2!1sen!2sus"
-                        width="600" height="450" style="border:0;" allowFullScreen="" loading="lazy"></iframe>
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d93091.5849207782!2d-77.6864382969578!3d43.18616013858725!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d6b3059614b353%3A0x5a001ffc4125e61e!2sRochester%2C%20NY!5e0!3m2!1sen!2sus!4v1652036864087!5m2!1sen!2sus"
+                        width="600" height="450" style={{border:0}} allowFullScreen="" loading="lazy"
+                        referrerPolicy="no-referrer-when-downgrade"></iframe>
                 </div>
                 <div className="contact-sect">
                     <form autoComplete="off" onSubmit={handleSubmit} action="https://formspree.io/f/mqkwpwoa" method="post" id="contact-form">
