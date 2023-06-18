a4aafd0e03908a779f9133da0b94db6204b186d7
Ben Jordan <bej9038@rit.edu>
2023-06-18 11:20:59 -0400

    no grid collapse

--- src/Components/MenuItem.js
+++ src/Components/MenuItem.js
@@ -1,36 +1,65 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {useState} from 'react'
 
 
 function MenuItem({menuItem, id, setId}) {
     const [expanded, setExpanded] = useState(false);
+    const [previousWindowPos, setPrev] = useState(0);
+
+    function customScroll()
+    {
+        // let interval = setInterval(function ()
+        // {
+        //     if(window.scrollY > 0)
+        //     {window.scroll({top: window.scrollY-1, behavior: 'instant'})}
+        //     else
+        //     {
+        //         clearInterval(interval)
+        //     }
+        //
+        // }, 1);
+        while(window.scrollY > 0)
+        {
+            window.scroll({top: window.scrollY-1, behavior: 'instant'})
+            // setTimeout(() => {
+            //     window.scroll({top: window.scrollY-1, behavior: 'instant'})
+            // }, 1);
+
+        }
+    }
     return (
-        <div className="MenuItem">
+        // <div className={`MenuItem ${expanded? 'collapsedGrid' : ''}`}>
+        <div className={`MenuItem`}>
             {
                 menuItem.map((item) =>
                 {
                     // if(id === -1 || item.id === id) {
                         return <div className={`portfolio ${expanded && item.id !== id ? 'fadeout' : 'fadein'}`} key={item.id}>
-                            <h5>
+                            <h5 className={`${id === -1 || item.id === id ? '' : 'collapsed'}`}>
                                 {item.title}
                             </h5>
-                            <div className="project-content">
+                            <div className={`project-content ${expanded ? 'expanded' : ''} ${id === -1 || item.id === id ? '' : 'collapsed'}`}>
                                 {/*<img className="img" src={item.image} alt=""/>*/}
                                 <div className="text-cover"></div>
-                                <p>
+                                <p className={`${id === -1 || item.id === id ? '' : 'collapsed'}`}>
                                     {item.description}
                                 </p>
-                                <button onClick={() => {
+                                <button className={`${id === -1 || item.id === id ? '' : 'collapsed'}`} onClick={() => {
                                     if(!expanded)
                                     {
-                                        setId(item.id) ;
+                                        // setPrev(window.scrollY)
+                                        // window.scroll({top: 0, behavior: 'instant'})
+                                        setId(item.id);
                                         setExpanded(true);
+
                                     }
                                     else
                                     {
                                         setId(-1);
                                         setExpanded(false);
+                                        // window.scroll({top: previousWindowPos, behavior: 'instant'})
+
                                     }
                                 }}>Expand</button>
                                 {/*<div className="hover-items">*/}
