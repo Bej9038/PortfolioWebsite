e8c7d02fe20881625651384d4332c4084457d859
Ben Jordan <bej9038@rit.edu>
2023-07-14 08:16:31 -0400

    resume

--- src/Components/ImageSection.js
+++ src/Components/ImageSection.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import resume from "../Downloads/Ben Jordan [July 2023 Resume] V7.pdf";
+import resume from "../Downloads/Ben Jordan [July 2023 Resume] V8.pdf";
 
 function ImageSection() {
     return (
