a186f1184ff5f1e39caf572bf543ec7f9a28ed3c
Ben Jordan <bej9038@rit.edu>
2024-02-23 22:11:55 -0700

    Started updating projects page

--- src/Components/ProjectList.js
+++ src/Components/ProjectList.js
@@ -32,23 +32,25 @@ import {faYoutube} from "@fortawesome/free-brands-svg-icons";
 import {faSoundcloud} from "@fortawesome/free-brands-svg-icons";
 import {faChrome} from "@fortawesome/free-brands-svg-icons";
 const portfolios = [
-    // {
-    //     id: 11,
-    //     category: ['2023'],
-    //     descriptions: [descriptions.temp],
-    //     title: 'Project Neuro',
-    // },
     {
-        id: 10,
+        id: 1,
         category: ['2023'],
-        descriptions: [descriptions.fv1, descriptions.fv2, descriptions.fv3, descriptions.fv4, descriptions.fv5],
-        title: 'FeatureViz - Deep Learning Visualization',
-        link:"https://github.com/Bej9038/FeatureViz",
+        link: 'https://google-research.github.io/seanet/musiclm/examples/',
+        icon: faChrome,
+        descriptions: [descriptions.synthlm1, descriptions.synthlm2, descriptions.synthlm3],
+        title: 'Entropy: Generative Audio Synthesizer',
+        images: [SynthLM3, SynthLM4],
+        imagecap: ["1. The structure of Google's MusicLM", "2. Some neat autoencoder audio compression"]
+    },
+    {
+        id: 2,
+        category: ['2023'],
+        link: 'https://github.com/emichael/dslabs',
         icon: faGithub,
-        images: [FV1, FV2, FV3],
-        imagecap: ["1: A cool looking feature from the third group of layers in ResNet50",
-            "2: A more basic feature from an earlier layer",
-            "3: A faint visualization of the stingray class from the final classifier"],
+        descriptions: [descriptions.dslabs1, descriptions.dslabs2, descriptions.dslabs3, descriptions.dslabs4],
+        images: [dslabs1, dslabs2],
+        imagecap: ["1: The Part-Time Parliament", "2: The greatest moment of my life"],
+        title: 'DSLabs - Sharded Key-Value Store'
     },
     {
         id: 7,
@@ -63,36 +65,30 @@ const portfolios = [
         title: 'EQ Audio Plugin'
     },
     {
-        id: 2,
+        id: 10,
         category: ['2023'],
-        link: 'https://github.com/emichael/dslabs',
+        descriptions: [descriptions.fv1, descriptions.fv2, descriptions.fv3, descriptions.fv4, descriptions.fv5],
+        title: 'FeatureViz - Deep Learning Visualization',
+        link:"https://github.com/Bej9038/FeatureViz",
         icon: faGithub,
-        descriptions: [descriptions.dslabs1, descriptions.dslabs2, descriptions.dslabs3, descriptions.dslabs4],
-        images: [dslabs1, dslabs2],
-        imagecap: ["1: The Part-Time Parliament", "2: The greatest moment of my life"],
-        title: 'DSLabs - Sharded Key-Value Store'
+        images: [FV1, FV2, FV3],
+        imagecap: ["1: A cool looking feature from the third group of layers in ResNet50",
+            "2: A more basic feature from an earlier layer",
+            "3: A faint visualization of the stingray class from the final classifier"],
     },
-    {
-        id: 1,
-        category: ['2023'],
-        link: 'https://google-research.github.io/seanet/musiclm/examples/',
-        icon: faChrome,
-        descriptions: [descriptions.synthlm1, descriptions.synthlm2, descriptions.synthlm3],
-        title: 'SynthLM',
-        images: [SynthLM3, SynthLM4],
-        imagecap: ["1. The structure of Google's MusicLM", "2. Some neat autoencoder audio compression"]
-    },
-    {
-        id: 4,
-        category: ['2020'],
-        link: 'https://github.com/Bej9038/ImplantTestingProgram',
-        icon: faGithub,
-        descriptions: [descriptions.ipt1, descriptions.ipt2, descriptions.ipt3, descriptions.ipt4],
-        images: [ITP1, ITP3, ITP4, ITP2],
-        imagecap: ["1: The main menu", "2: Inharmonicty training",
-            "3: Setting the frequency ranges on the canvas's axes and creating the oscillators", "4: Speech to noise ratio selection"],
-        title: 'Implant Testing Program'
-    },
+
+
+    // {
+    //     id: 4,
+    //     category: ['2020'],
+    //     link: 'https://github.com/Bej9038/ImplantTestingProgram',
+    //     icon: faGithub,
+    //     descriptions: [descriptions.ipt1, descriptions.ipt2, descriptions.ipt3, descriptions.ipt4],
+    //     images: [ITP1, ITP3, ITP4, ITP2],
+    //     imagecap: ["1: The main menu", "2: Inharmonicty training",
+    //         "3: Setting the frequency ranges on the canvas's axes and creating the oscillators", "4: Speech to noise ratio selection"],
+    //     title: 'Implant Testing Program'
+    // },
     {
         id: 3,
         category: ['2022'],
@@ -103,42 +99,42 @@ const portfolios = [
         imagecap: ["1: Part of the initial project specifications", "2: Welcome menu","3: Playing around with the controls"],
         title: 'Spatial Attributes Evaluation'
     },
-    {
-        id: 8,
-        category: ['2020'],
-        link: 'https://github.com/Bej9038/OmnitoneV2',
-        icon: faGithub,
-        descriptions: [descriptions.omni1, descriptions.omni2, descriptions.omni3],
-        audio: [dry, wet, wet2],
-        audiocap:["The original audio file", "The audio file being played inside the simulated savings bank"],
-        images: [OMNI1, OMNI2, OMNI4],
-        imagecap: ["1: The Omnitone interface displaying the Rochester savings bank. " +
-        "Users can use their mouse to explore the VR room and its audio characteristics.",
-            "2: The second selectable room"],
-        title: 'Omnitone'
-    },
-    {
-        id: 5,
-        category: ['2022'],
-        link: 'https://github.com/Bej9038/GraphSAGE-ML-Project',
-        icon: faGithub,
-        descriptions: [descriptions.gnn1, descriptions.gnn2, descriptions.gnn3, descriptions.gnn4, descriptions.gnn5],
-        images: [GNN2, GNN1],
-        imagecap: [
-            "1: A GraphSAGE layer visualized",
-            "2: Visualization of the CORA dataset. It consists of 2708 scientific publications classified " +
-        "into one of seven classes. The citation network consists of 5429 directed links between papers.",
-            ],
-        title: 'Graph Neural Network Research Project'
-    },
-    {
-        id: 6,
-        category: ['2021'],
-        link: 'https://github.com/Bej9038/PortfolioWebsite',
-        icon: faGithub,
-        descriptions: [descriptions.portfolio1],
-        title: 'Portfolio Website'
-    },
+    // {
+    //     id: 8,
+    //     category: ['2020'],
+    //     link: 'https://github.com/Bej9038/OmnitoneV2',
+    //     icon: faGithub,
+    //     descriptions: [descriptions.omni1, descriptions.omni2, descriptions.omni3],
+    //     audio: [dry, wet, wet2],
+    //     audiocap:["The original audio file", "The audio file being played inside the simulated savings bank"],
+    //     images: [OMNI1, OMNI2, OMNI4],
+    //     imagecap: ["1: The Omnitone interface displaying the Rochester savings bank. " +
+    //     "Users can use their mouse to explore the VR room and its audio characteristics.",
+    //         "2: The second selectable room"],
+    //     title: 'Omnitone'
+    // },
+    // {
+    //     id: 5,
+    //     category: ['2022'],
+    //     link: 'https://github.com/Bej9038/GraphSAGE-ML-Project',
+    //     icon: faGithub,
+    //     descriptions: [descriptions.gnn1, descriptions.gnn2, descriptions.gnn3, descriptions.gnn4, descriptions.gnn5],
+    //     images: [GNN2, GNN1],
+    //     imagecap: [
+    //         "1: A GraphSAGE layer visualized",
+    //         "2: Visualization of the CORA dataset. It consists of 2708 scientific publications classified " +
+    //     "into one of seven classes. The citation network consists of 5429 directed links between papers.",
+    //         ],
+    //     title: 'Graph Neural Network Research Project'
+    // },
+    // {
+    //     id: 6,
+    //     category: ['2021'],
+    //     link: 'https://github.com/Bej9038/PortfolioWebsite',
+    //     icon: faGithub,
+    //     descriptions: [descriptions.portfolio1],
+    //     title: 'Portfolio Website'
+    // },
     // {
     //     id: 9,
     //     category: ['2022'],
