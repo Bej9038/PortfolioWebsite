1aef5d9bc859cc4a4a8ae4499f52ac71fdb8c8d0
Ben Jordan <bej9038@rit.edu>
2024-09-08 10:31:50 -0600

    loaction

--- src/Pages/ContactPage.js
+++ src/Pages/ContactPage.js
@@ -16,15 +16,16 @@ function ContactPage() {
             <div className="sections">
                 <div className="map-sect">
                     <iframe
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d47108.74706434036!2d-76.49837495!3d42.44270305!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d08182e0af88f7%3A0xae52768a56ece74!2sIthaca%2C%20NY!5e0!3m2!1sen!2sus!4v1686931967031!5m2!1sen!2sus"
-                        width="600" height="450" style={{border:0}} allowFullScreen="" loading="lazy"
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d162657.64342739235!2d-104.8104645841777!3d39.74629137167686!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876b80aa231f17cf%3A0x118ef4f8278a36d6!2sDenver%2C%20CO!5e0!3m2!1sen!2sus!4v1725813029847!5m2!1sen!2sus"
+                        width="600" height="450" style="border:0;" allowFullScreen="" loading="lazy"
                         referrerPolicy="no-referrer-when-downgrade"></iframe>
                 </div>
                 <div className="contact-sect">
-                    <form autoComplete="off" onSubmit={handleSubmit} action="https://formspree.io/f/mqkwpwoa" method="post" id="contact-form">
+                    <form autoComplete="off" onSubmit={handleSubmit} action="https://formspree.io/f/mqkwpwoa"
+                          method="post" id="contact-form">
                         <div className="form-group">
                             <label htmlFor="name" className="label">Name</label>
-                            <input autoComplete="new-password" type="text" id = "name" className="textio" name = "name" />
+                            <input autoComplete="new-password" type="text" id="name" className="textio" name = "name" />
                         </div>
                         <input type="hidden" value = 'prayer'/>
 
