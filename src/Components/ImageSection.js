8636810836613eb40af266d74fad5f1b74e9622c
bejordae <bej9@cornell.edu>
2025-10-27 21:47:33 -0400

    Website updates

--- src/Components/ImageSection.js
+++ src/Components/ImageSection.js
@@ -8,14 +8,13 @@ function ImageSection() {
             <div className="image-content">
                 <div className="about-info">
                     <p className="about-text">
-                        <span>I am a graduate of Cornell & RIT currently working as an engineer at Amazon.</span>
+                        <span>I am a graduate of Cornell & RIT currently working as a software engineer at Amazon.</span>
                         <br/><br/>
                         In my current job, I work on large scale software systems with project focus on complex spark ETL jobs and ML ops.
+                        I have a background in ML from my masters as well as from my previous role and internship.
                         <br/><br/>
-                        I also have domain experience in machine learning from my masters as well as from my previous role and internship working as an MLE.
-                        <br/><br/>
-                        Outside of work, I like to work on my project, EntropyAudio, that aims to redefine how we create sound for music composition.
-                        I also love trying new food, cool architecture, and keeping up with the latest trends in machine learning and machine learning systems.
+                        Outside of work I like to spend my time on my project, Entropy Audio, that aims to redefine how we create sound for music composition.
+                        I also love trying new food, cool architecture, and keeping up with the latest trends in ML and ML systems.
                     </p>
                 </div>
                 <div className="about-img-wrapper">
@@ -26,9 +25,9 @@ function ImageSection() {
                     />
                 </div>
             </div>
-            <a className="download-link" style={{color: "var(--font-color2)"}} href={resume} download>
-                <button className="btn">Download CV</button>
-            </a>
+            {/*<a className="download-link" style={{color: "var(--font-color2)"}} href={resume} download>*/}
+            {/*    <button className="btn">Download CV</button>*/}
+            {/*</a>*/}
         </div>
     )
 }
--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -2,25 +2,17 @@ import React from 'react';
 import Title from "../Components/Title";
 import Skills from "../Components/Skills";
 import ImageSection from "../Components/ImageSection";
+import resume from "../Downloads/Benjamin_Jordan_Resume_2025.pdf";
 
 function AboutPage() {
     return (
         <div className="AboutPage">
             <Title title={'Ben Jordan'} progress={25}/>
             <ImageSection />
-            {/*<Title title={'Skills'} progress={50}/>*/}
-            {/*<div className="skills-group">*/}
-            {/*    <Skills skill = {'Python'} progress={'100%'}/>*/}
-            {/*    <Skills skill = {'PyTorch'} progress={'100%'}/>*/}
-            {/*    <Skills skill = {'NumPy'} progress={'100%'}/>*/}
-            {/*    <Skills skill = {'C++'} progress={'100%'}/>*/}
-            {/*    <Skills skill = {'C'} progress={'100%'}/>*/}
-            {/*    <Skills skill = {'CUDA'} progress={'100%'}/>*/}
-            {/*    <Skills skill = {'Docker'} progress={'100%'}/>*/}
-            {/*    <Skills skill = {'Git'} progress={'100%'}/>*/}
-            {/*    <Skills skill = {'Linux'} progress={'100%'}/>*/}
-            {/*    <Skills skill = {'LaTeX'} progress={'100%'}/>*/}
-            {/*</div>*/}
+            <Title title={'CV'} progress={50}/>
+            <a className="download-link" style={{color: "var(--font-color2)"}} href={resume} download>
+                <button className="btn">Download</button>
+            </a>
         </div>
     );
 }
