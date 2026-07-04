5031bf8c32871e88082c6245ca49cc0b8d3aebe8
bejordae <bej9@cornell.edu>
2026-07-04 16:41:51 -0400

    Update wording

--- src/Components/ImageSection.js
+++ src/Components/ImageSection.js
@@ -11,7 +11,7 @@ function ImageSection() {
                         I am currently an engineer at Amazon Ads on the Brand Lift team (Measurement, Ad Tech, Data Science org).
                         Previously, I worked on computer vision at KLA and at Northrop Grumman's Autonomous Intelligence and Robotics Lab.</span>
                         <br/><br/>
-                        The areas within ML that I'm most interested in are flow/diffusion models, reinforcement learning, and ML hardware & frameworks.
+                        The areas within ML that interest me the most are flow/diffusion models, reinforcement learning, and ML hardware & frameworks.
                         <br/><br/>
                         In my free time, I'm working on Entropy Audio, a project which aims to redefine how composers design sound.
                     </p>
